c7ed057c07326c915df4da31deb0f1aab0fa7736
Ben <benawhiting@gmail.com>
2022-07-20 14:53:06 -0700

    Fix minor bugs

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const module = (() => {
         let winnerAlert = document.querySelector(".announce-winner");
         let turn = 0;
 
-        let players = ["", ""];
+        let firstPlayerDisplay = document.querySelector(".player1");
+        let secondPlayerDisplay = document.querySelector(".player2"); 
+
+        let players = ["Player 1", "Player 2"];
 
         let squares = [
             "",
@@ -19,6 +22,7 @@ const module = (() => {
             "",
         ]
 
+        // Set player names
         let playerNames = (() => {
             let firstPlayer = document.querySelector("#player1-name");
             let secondPlayer = document.querySelector("#player2-name");
@@ -28,14 +32,17 @@ const module = (() => {
             firstPlayerSubmit.addEventListener('click', () => {
                 players[0] = firstPlayer.value;
                 firstPlayer.value = "";
+                firstPlayerDisplay.textContent = `${players[0]} (x's)`;
             });
 
             secondPlayerSubmit.addEventListener('click', () => {
                 players[1] = secondPlayer.value;
                 secondPlayer.value = "";
+                secondPlayerDisplay.textContent = `${players[1]} (o's)`;
             })
         })();
 
+        // Creates logic for the game
         const gameLogic = (() => {
             let winner = false;
 
@@ -49,19 +56,14 @@ const module = (() => {
 
                 // Updates the squares[] property at the correlated index of the square clicked
                 move.addEventListener('click', (event) => {
-                    console.log(squares);
-
                     if (squares[Number(move.id)] != "" || winner == true) {
-                        resetGrid();
                         return;
                     } else if (turn % 2 == 0) {
-                        resetGrid();
                         squares.splice(Number(move.id), 1, "x");
                         move.innerText = squares[Number(move.id)];
                         turn++;
                         gameOver();
                     } else {
-                        resetGrid();
                         squares.splice(Number(move.id), 1, "o");
                         move.innerText = squares[Number(move.id)];
                         turn++;
@@ -72,8 +74,6 @@ const module = (() => {
                 // Resets the grid display and squares[]
                 const resetGrid = (() => {
                     reset.addEventListener('click', () => {
-                        console.log("Here");
-        
                         squares = [
                             "",
                             "",
@@ -86,60 +86,62 @@ const module = (() => {
                             "",
                         ]
         
-                        move.innerText = "";
+                        move.innerText = squares[Number(move.id)];
+                        winnerAlert.textContent = "";
+                        turn = 0;
                     });
-                });
+                })();
             }
 
             // Check for a winner of the game
             const gameOver = () => {
                 if (squares[0] == "x" && squares[1] == "x" && squares[2] == "x") {
-                    winnerAlert.innerText = "Player 1 Wins!";
+                    winnerAlert.innerText = `${players[0]} Wins!`;
                     winner = true;
                 } else if (squares[3] == "x" && squares[4] == "x" && squares[5] == "x") {
-                    winnerAlert.innerText = "Player 1 Wins!";
+                    winnerAlert.innerText = `${players[0]} Wins!`;
                     winner = true;
                 } else if (squares[6] == "x" && squares[7] == "x" && squares[8] == "x") {
-                    winnerAlert.innerText = "Player 1 Wins!";
+                    winnerAlert.innerText = `${players[0]} Wins!`;
                     winner = true;
                 } else if (squares[0] == "x" && squares[3] == "x" && squares[6] == "x") {
-                    winnerAlert.innerText = "Player 1 Wins!";
+                    winnerAlert.innerText = `${players[0]} Wins!`;
                     winner = true;
                 } else if (squares[1] == "x" && squares[4] == "x" && squares[7] == "x") {
-                    winnerAlert.innerText = "Player 1 Wins!";
+                    winnerAlert.innerText = `${players[0]} Wins!`;
                     winner = true;
                 } else if (squares[2] == "x" && squares[5] == "x" && squares[8] == "x") {
-                    winnerAlert.innerText = "Player 1 Wins!";
+                    winnerAlert.innerText = `${players[0]} Wins!`;
                     winner = true;
                 } else if (squares[0] == "x" && squares[4] == "x" && squares[8] == "x") {
-                    winnerAlert.innerText = "Player 1 Wins!";
+                    winnerAlert.innerText = `${players[0]} Wins!`;
                     winner = true;
                 } else if (squares[2] == "x" && squares[4] == "x" && squares[6] == "x") {
-                    winnerAlert.innerText = "Player 1 Wins!";
+                    winnerAlert.innerText = `${players[0]} Wins!`;
                     winner = true;
                 } else if (squares[0] == "o" && squares[1] == "o" && squares[2] == "o") {
-                    winnerAlert.innerText = "Player 2 Wins!";
+                    winnerAlert.innerText = `${players[1]} Wins!`;
                     winner = true;
                 } else if (squares[3] == "o" && squares[4] == "o" && squares[5] == "o") {
-                    winnerAlert.innerText = "Player 2 Wins!";
+                    winnerAlert.innerText = `${players[1]} Wins!`;
                     winner = true;
                 } else if (squares[6] == "o" && squares[7] == "o" && squares[8] == "o") {
-                    winnerAlert.innerText = "Player 2 Wins!";
+                    winnerAlert.innerText = `${players[1]} Wins!`;
                     winner = true;
                 } else if (squares[0] == "o" && squares[3] == "o" && squares[6] == "o") {
-                    winnerAlert.innerText = "Player 2 Wins!";
+                    winnerAlert.innerText = `${players[1]} Wins!`;
                     winner = true;
                 } else if (squares[1] == "o" && squares[4] == "o" && squares[7] == "o") {
-                    winnerAlert.innerText = "Player 2 Wins!";
+                    winnerAlert.innerText = `${players[1]} Wins!`;
                     winner = true;
                 } else if (squares[2] == "o" && squares[5] == "o" && squares[8] == "o") {
-                    winnerAlert.innerText = "Player 2 Wins!";
+                    winnerAlert.innerText = `${players[1]} Wins!`;
                     winner = true;
                 } else if (squares[0] == "o" && squares[4] == "o" && squares[8] == "o") {
-                    winnerAlert.innerText = "Player 2 Wins!";
+                    winnerAlert.innerText = `${players[1]} Wins!`;
                     winner = true;
                 } else if (squares[2] == "o" && squares[4] == "o" && squares[6] == "o") {
-                    winnerAlert.innerText = "Player 2 Wins!";
+                    winnerAlert.innerText = `${players[1]} Wins!`;
                     winner = true;
                 } else if (squares.includes("") == false) {
                     alert("Tie!");
